d18daae172800aef73436d28e33213d07b46220e
eskomj <esko.jaakkola@gmail.com>
2016-04-13 22:29:59 +0300

    LightController header file

---
+++ harjoitus8/include/util/LightController.h
@@ -0,0 +1,13 @@
+/*
+ * LightController.h
+ *
+ *  Created on: Apr 13, 2016
+ *      Author: esko
+ */
+
+#ifndef INCLUDE_UTIL_LIGHTCONTROLLER_H_
+#define INCLUDE_UTIL_LIGHTCONTROLLER_H_
+
+typedef enum states{LIGHT_OFF, LIGHT_ON, LIGHT_STATE_UNKNOWN} states;
+
+#endif /* INCLUDE_UTIL_LIGHTCONTROLLER_H_ */
