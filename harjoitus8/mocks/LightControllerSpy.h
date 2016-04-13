d06d935bd530b0bad92ed883c3fde8b3597a29c3
eskomj <esko.jaakkola@gmail.com>
2016-04-13 22:30:52 +0300

    parametri korjattu.

--- harjoitus8/mocks/LightControllerSpy.h
+++ harjoitus8/mocks/LightControllerSpy.h
@@ -1,6 +1,8 @@
 #ifndef D_LightControllerSpy_H
 #define D_LightControllerSpy_H
 
+#include "LightController.h"
+
 /**********************************************************
  *
  * LightControllerSpy is responsible for ...
@@ -10,8 +12,8 @@
 void LightControllerSpy_Create(void);
 void LightControllerSpy_Destroy(void);
 
-void LightControllerSpy_On(void);
-void LightControllerSpy_Off(void);
+void LightControllerSpy_On(int id);
+void LightControllerSpy_Off(int id);
 void LightControllerSpy_GetLastId(void);
 void LightControllerSpy_GetState(void);
 
