cc8511d83603c4689198730cb3342773610f2e92
eskomj <esko.jaakkola@gmail.com>
2016-04-14 20:05:15 +0300

    Lisätty metodeita.

--- harjoitus8/mocks/LightControllerSpy.c
+++ harjoitus8/mocks/LightControllerSpy.c
@@ -1,11 +1,23 @@
 #include "LightControllerSpy.h"
 
+int lastId, lastLevel;
+
 void LightControllerSpy_Create(void)
 {
 }
 
 void LightControllerSpy_Destroy(void)
 {
+}
+
+int LightControllerSpy_GetLastId(void)
+{
+    return lastId;
+}
+
+int LightControllerSpy_GetLastState(void)
+{
+    return lastLevel;
 }
 
 
