e7ca78c57b347f18bb11a951211b6ca57b43af87
eskomj <esko.jaakkola@gmail.com>
2016-04-15 21:30:20 +0300

    Poistettu tilat täältä.

--- harjoitus8/include/util/LightController.h
+++ harjoitus8/include/util/LightController.h
@@ -8,12 +8,6 @@
 #ifndef INCLUDE_UTIL_LIGHTCONTROLLER_H_
 #define INCLUDE_UTIL_LIGHTCONTROLLER_H_
 
-typedef enum
-{
-	LIGHT_ID_UNKNOWN = -1, LIGHT_STATE_UNKNOWN = -1,
-	LIGHT_OFF = 0, LIGHT_ON = 1
-} LightState;
-
 void LightController_TurnOn(int id);
 void LightController_TurnOff(int id);
 
