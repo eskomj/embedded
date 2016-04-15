ca1a1bf5591f09169d006348863f5b39c8a22acc
eskomj <esko.jaakkola@gmail.com>
2016-04-15 21:29:25 +0300

    Lisätty metodit turnOn() ja turn(off)

--- harjoitus8/mocks/LightControllerSpy.c
+++ harjoitus8/mocks/LightControllerSpy.c
@@ -4,6 +4,8 @@ int lastId, lastLevel;
 
 void LightControllerSpy_Create(void)
 {
+	lastId = LIGHT_ID_UNKNOWN;
+	lastLevel = LIGHT_STATE_UNKNOWN;
 }
 
 void LightControllerSpy_Destroy(void)
@@ -18,6 +20,18 @@ int LightControllerSpy_GetLastId(void)
 int LightControllerSpy_GetLastState(void)
 {
     return lastLevel;
+}
+
+void LightController_TurnOn(int id)
+{
+	lastId = id;
+	lastLevel = LIGHT_ON;
+}
+
+void LightController_TurnOff(int id)
+{
+	lastId = id;
+	lastLevel = LIGHT_OFF;
 }
 
 
