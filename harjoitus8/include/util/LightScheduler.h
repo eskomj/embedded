75fb08b95600e6023260b204441c4b14c42db0ca
eskomj <esko.jaakkola@gmail.com>
2016-04-17 20:20:02 +0300

    Lisätty wakeup() metodi.

--- harjoitus8/include/util/LightScheduler.h
+++ harjoitus8/include/util/LightScheduler.h
@@ -12,5 +12,6 @@
 void LightScheduler_Create(void);
 void LightScheduler_Destroy(void);
 void LightScheduler_ScheduleTurnOn(int id, int day, int minute);
+void LightScheduler_Wakeup(void);
 
 #endif  /* D_FakeLightScheduler_H */
