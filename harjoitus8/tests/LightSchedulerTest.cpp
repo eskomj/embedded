b1864a89856058f00cd3861e56513c146b4334a8
eskomj <esko.jaakkola@gmail.com>
2016-04-17 20:20:48 +0300

    Täydennetty testejä.

--- harjoitus8/tests/LightSchedulerTest.cpp
+++ harjoitus8/tests/LightSchedulerTest.cpp
@@ -1,5 +1,4 @@
-extern "C"
-{
+extern "C" {
 #include "LightScheduler.h"
 #include "LightControllerSpy.h"
 #include "FakeTimeService.h"
@@ -7,34 +6,47 @@ extern "C"
 }
 #include "CppUTest/TestHarness.h"
 
-
-
 TEST_GROUP(LightScheduler)
 {
-    void setup()
-    {
-      LightScheduler_Create();
-    }
+	void setup() {
+		LightScheduler_Create();
+		LightControllerSpy_Create();
+	}
 
-    void teardown()
-    {
-       LightScheduler_Destroy();
-    }
+	void teardown() {
+		LightScheduler_Destroy();
+		LightControllerSpy_Destroy();
+	}
 };
 
 TEST(LightScheduler, ScheduleOnEverydayNotTimeYet)
 {
-  LightScheduler_ScheduleTurnOn(3, EVERYDAY, 1200);
-  //FakeTimeService_SetDay(MONDAY);
-  //FakeTimeService_SetMinute(1199);
-}
+	LightScheduler_ScheduleTurnOn(3, EVERYDAY, 1200);
+	FakeTimeService_SetDay(MONDAY);
+	FakeTimeService_SetMinute(1199);
 
+	LightScheduler_Wakeup();
+
+	LONGS_EQUAL(LIGHT_ID_UNKNOWN, LightControllerSpy_GetLastId());
+	LONGS_EQUAL(LIGHT_STATE_UNKNOWN, LightControllerSpy_GetLastState());
+}
 
 TEST(LightScheduler, NoChangeToLightsDuringInitialization)
 {
-    LONGS_EQUAL(LIGHT_ID_UNKNOWN, LightControllerSpy_GetLastId());
-    LONGS_EQUAL(LIGHT_STATE_UNKNOWN, LightControllerSpy_GetLastState());
+	LONGS_EQUAL(LIGHT_ID_UNKNOWN, LightControllerSpy_GetLastId());
+	LONGS_EQUAL(LIGHT_STATE_UNKNOWN, LightControllerSpy_GetLastState());
 }
 
+TEST(LightScheduler, ScheduleOnEverydayItsTime)
+{
+	LightScheduler_ScheduleTurnOn(3, EVERYDAY, 1200);
+	FakeTimeService_SetDay(MONDAY);
+	FakeTimeService_SetMinute(1200);
+
+	LightScheduler_Wakeup();
+
+	LONGS_EQUAL(3, LightControllerSpy_GetLastId());
+	LONGS_EQUAL(LIGHT_ON, LightControllerSpy_GetLastState());
+}
 
 
