c483a72aa0e1224076f60c788a865896e3527ecf
eskomj <esko.jaakkola@gmail.com>
2016-04-28 19:14:09 +0300

    Korjattu metodinimi.

--- harjoitus8/include/util/LightScheduler.h
+++ harjoitus8/include/util/LightScheduler.h
@@ -13,6 +13,6 @@ void LightScheduler_Create(void);
 void LightScheduler_Destroy(void);
 void LightScheduler_ScheduleTurnOn(int id, int day, int minute);
 void LightScheduler_ScheduleTurnOff(int id, int day, int minute);
-void LightScheduler_Wakeup(void);
+void LightScheduler_WakeUp(void);
 
 #endif  /* D_FakeLightScheduler_H */
